7502c1d91e43a717303a2572bdb27ff04d6e03dc
oasisers99 <oasisers99@yahoo.com>
2017-07-23 23:44:06 +0930

    Create Tribe

---
+++ app/Http/Middleware/RedirectIfNotLoggedIn.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: songminseok
+ * Date: 23/7/17
+ * Time: 11:23 PM
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfNotLoggedIn
+{
+
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (!Auth::check()) {
+            return redirect(route('auth.login'));
+        }
+
+        return $next($request);
+    }
+}
