054eeea70dc56221598881328d94e4d3db7cd468
oasisers99 <oasisers99@yahoo.com>
2017-11-07 22:55:55 +1030

    config

---
+++ config/code.php
@@ -0,0 +1,15 @@
+<?php
+
+	
+return [
+    'interests' => [
+        '1' => 'Arts',
+        '2' => 'Alumni',
+        '3' => 'Culture',
+        '4' => 'Nationalities',
+        '5' => 'Location',
+        '6' => 'Professions',
+        '7' => 'Recreation',
+        '8' => 'Sports'
+    ]
+];
